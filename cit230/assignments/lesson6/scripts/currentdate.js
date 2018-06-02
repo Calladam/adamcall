29a07268a981dfe1abc2e622c4b47a2e919f5dfc
Calladam <38711413+Calladam@users.noreply.github.com>
2018-06-02 01:26:24 -0600

    Add files via upload

---
+++ cit230/assignments/lesson6/scripts/currentdate.js
@@ -0,0 +1,10 @@
+   d = new Date();
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.getElementById("currentDate").innerHTML = weekday[d.getDay()] + ", " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
+    
+    
+    
+    
+    
+    
